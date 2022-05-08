03df0f34e218ff0cbc2e8037411113ed2c84c4e5
Ireshaprabodhani <84035631+Ireshaprabodhani@users.noreply.github.com>
2022-05-08 23:57:58 +0530

    Add category

--- app/src/main/java/com/company/eat24/MenuActivity.java
+++ app/src/main/java/com/company/eat24/MenuActivity.java
@@ -1,4 +1,14 @@
 package com.company.eat24;
 
-public class MenuActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
 }
